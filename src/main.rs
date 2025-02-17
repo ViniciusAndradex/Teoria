60dab0cfa2ab1d1b2f9e3b6bf1dba51a81a14f59
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2025-02-17 03:10:09 -0300

    tentantiva de set \d e a-z.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref SPECIAL_NUMS: Mutex<Vec<i32>> = Mutex::new(vec![0]);
+    static ref ALPHA: Mutex<Vec<char>> = Mutex::new(vec!['a']);
+}
 fn replace(original: &str, regex: &str, to: &str) -> String {
     let alter = regex_analizy(regex).unwrap();
     println!("{}", alter.0.clone().into_iter().collect::<String>());
@@ -57,15 +64,18 @@ fn identify_word(original: &str, alter: Vec<char>, regex: &Vec<Regex>) -> Vec<us
     let last_char = alter.len() - 1;
     let mut agrupar: bool = false;
     let mut fecho: bool = false;
+    let mut set: bool = false;
 
     for t in regex {
         match t {
             Regex::Fecho => fecho = true,
             Regex::Agrupar => agrupar = true,
+            Regex::Set => set = true,
         }
     }
     for (pos, letter) in original.chars().enumerate() {
         if count <= alter.len() - 1 {
+            if set && letter == '#' {}
             if letter == alter[count] {
                 result.push(pos);
                 count += 1;
@@ -90,33 +100,80 @@ fn identify_word(original: &str, alter: Vec<char>, regex: &Vec<Regex>) -> Vec<us
 enum Regex {
     Fecho,
     Agrupar,
+    Set,
+}
+fn history_values(one: char, two: char) {
+    if one == '\\' && two == 'd' {
+        let mut special = SPECIAL_NUMS.lock().unwrap();
+        special.extend(0..9);
+    } else {
+        let mut alp = ALPHA.lock().unwrap();
+        alp.extend(one..two);
+    }
 }
 
 fn regex_analizy(regex: &str) -> Option<(Vec<char>, Vec<Regex>)> {
     let mut vec_regex: Vec<char> = regex.chars().collect();
     let mut get_symbols: Vec<Regex> = Vec::new();
-    let mut indexes: Vec<usize> = Vec::new();
-    for (index, symbol) in vec_regex.clone().iter().enumerate() {
-        if *symbol == '*' {
-            indexes.push(index);
-            get_symbols.push(Regex::Fecho);
-        }
-
-        if *symbol == '(' || *symbol == ')' {
-            if *symbol == '(' {
-                indexes.push(index);
-            } else {
-                indexes.push(index);
-                get_symbols.push(Regex::Agrupar)
+    let mut indexes_set: Vec<char> = Vec::new();
+    let mut agrupar = false;
+    let mut fecho = false;
+    let mut set = false;
+    let mut loop_count = 0;
+    loop {
+        for (index, symbol) in vec_regex.clone().iter().enumerate() {
+            if *symbol == '*' {
+                if loop_count == 2 {
+                    vec_regex.remove(index);
+                }
+                fecho = true;
             }
+
+            if *symbol == '(' || *symbol == ')' {
+                if loop_count == 1 {
+                    vec_regex.remove(index);
+                }
+                agrupar = true;
+            }
+            if *symbol == '[' || *symbol == ']' {
+                if loop_count == 0 {
+                    if *symbol == '[' {
+                        indexes_set.push(vec_regex[index + 1]);
+                        if vec_regex[index + 1] == '\\' {
+                            vec_regex.insert(index - 1, '%');
+                        } else {
+                            vec_regex.insert(index - 1, '#')
+                        }
+                    } else {
+                        indexes_set.push(vec_regex[index - 1]);
+                        if vec_regex[index - 3] == '[' {
+                            vec_regex.drain((index - 3)..index);
+                        } else {
+                            vec_regex.drain((index - 2)..index);
+                        }
+                    }
+                }
+                set = true;
+            }
+        }
+        loop_count += 1;
+        if loop_count > 2 {
+            break;
         }
     }
 
-    if !indexes.is_empty() {
-        for (ind, value) in indexes.iter().enumerate() {
-            let index = value - ind;
-            vec_regex.remove(index);
-        }
+    if !indexes_set.is_empty() {
+        history_values(indexes_set[0], indexes_set[1]);
+    }
+
+    if fecho {
+        get_symbols.push(Regex::Fecho);
+    }
+    if agrupar {
+        get_symbols.push(Regex::Agrupar);
+    }
+    if set {
+        get_symbols.push(Regex::Set);
     }
     if get_symbols.is_empty() {
         return None;
