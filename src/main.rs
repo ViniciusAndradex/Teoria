f61ec7d21f02389f1211f9b173a8e3cc14b94bcc
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2025-02-17 01:46:35 -0300

    agrupar implementado.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn replace(original: &str, regex: &str, to: &str) -> String {
-    let alter = regex_analizy(regex).unwrap().to_vec();
-    replacement(original, alter, to)
+    let alter = regex_analizy(regex).unwrap();
+    println!("{}", alter.0.clone().into_iter().collect::<String>());
+    replacement(original, alter.0, to, alter.1)
 }
 
-fn replacement(original: &str, alter: Vec<char>, to: &str) -> String {
+fn replacement(original: &str, alter: Vec<char>, to: &str, regex: Vec<Regex>) -> String {
     let mut result = original.to_string();
-    let words_index = identify_word(&result, alter.clone());
+    let words_index = identify_word(&result, alter.clone(), &regex);
     let letter_to: Vec<char> = to.chars().collect();
     let mut origin_vec: Vec<char> = result.chars().collect();
     if words_index.is_empty() {
@@ -47,42 +48,83 @@ fn replacement(original: &str, alter: Vec<char>, to: &str) -> String {
         }
     }
     result = origin_vec.iter().collect();
-    replacement(&result, alter.clone(), to)
+    replacement(&result, alter.clone(), to, regex)
 }
 
-fn identify_word(original: &str, alter: Vec<char>) -> Vec<usize> {
+fn identify_word(original: &str, alter: Vec<char>, regex: &Vec<Regex>) -> Vec<usize> {
     let mut result: Vec<usize> = Vec::new();
     let mut count: usize = 0;
-    let last_char = alter.len() - 2;
+    let last_char = alter.len() - 1;
+    let mut agrupar: bool = false;
+    let mut fecho: bool = false;
+
+    for t in regex {
+        match t {
+            Regex::Fecho => fecho = true,
+            Regex::Agrupar => agrupar = true,
+        }
+    }
     for (pos, letter) in original.chars().enumerate() {
-        if count < alter.len() - 1 {
+        if count <= alter.len() - 1 {
             if letter == alter[count] {
                 result.push(pos);
                 count += 1;
             }
-        } else if letter == alter[last_char] && count != 0 {
+            if count == alter.len() - 1 {
+                count += 1;
+            }
+        } else if letter == alter[last_char] && count != 0 && fecho && !agrupar {
             result.push(pos);
-            count += 1;
         } else if count > 0 {
             break;
         }
     }
 
+    if result.len() <= alter.len() - 2 {
+        result.clear();
+    }
+
     result
 }
 
-fn regex_analizy(regex: &str) -> Option<Vec<char>> {
-    let vec_regex: Vec<char> = regex.chars().collect();
+enum Regex {
+    Fecho,
+    Agrupar,
+}
 
-    for symbol in vec_regex.clone() {
-        if symbol == '*' {
-            return Some(vec_regex);
+fn regex_analizy(regex: &str) -> Option<(Vec<char>, Vec<Regex>)> {
+    let mut vec_regex: Vec<char> = regex.chars().collect();
+    let mut get_symbols: Vec<Regex> = Vec::new();
+    let mut indexes: Vec<usize> = Vec::new();
+    for (index, symbol) in vec_regex.clone().iter().enumerate() {
+        if *symbol == '*' {
+            indexes.push(index);
+            get_symbols.push(Regex::Fecho);
+        }
+
+        if *symbol == '(' || *symbol == ')' {
+            if *symbol == '(' {
+                indexes.push(index);
+            } else {
+                indexes.push(index);
+                get_symbols.push(Regex::Agrupar)
+            }
         }
     }
-    return None;
+
+    if !indexes.is_empty() {
+        for (ind, value) in indexes.iter().enumerate() {
+            let index = value - ind;
+            vec_regex.remove(index);
+        }
+    }
+    if get_symbols.is_empty() {
+        return None;
+    }
+    Some((vec_regex, get_symbols))
 }
 
 fn main() {
-    let word = replace("fun function funnnnção", "por*", "teste");
+    let word = replace("fun function funnnnção furia fu f", "fun*", "teste");
     println!("Word: {}", word);
 }
